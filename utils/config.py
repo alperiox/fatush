f88d182a5a862c5bd1b55f9ef4f8eadca605b998
Alper Balbay <itsc0508@gmail.com>
2024-02-20 01:01:13 +0300

    feat: added manual configuration file path setting

--- utils/config.py
+++ utils/config.py
@@ -5,23 +5,27 @@ import yaml
 CWD = os.getcwd()
 CONFIG_PATH = os.path.join(CWD, "config.yaml")
 
-def add_config(key, value):
-    conf = load_config()
+
+def add_config(key, value, path=CONFIG_PATH):
+    conf = load_config(path=path)
     conf[key] = value
 
-    with open(CONFIG_PATH, "w") as f:
+    with open(path, "w") as f:
         yaml.dump(conf, f)
 
-def remove_config(key, value):
-    conf = load_config()
+
+def remove_config(key, path=CONFIG_PATH):
+    conf = load_config(path=path)
     conf.pop(key)
 
-def load_config():
-    with open(CONFIG_PATH, "r") as f:
+
+def load_config(path: str = CONFIG_PATH):
+    with open(path, "r") as f:
         config = yaml.safe_load(f)
     return config
 
-def create_config():
-    with open(CONFIG_PATH, "w") as f:
+
+def create_config(path: str = CONFIG_PATH):
+    with open(path, "w") as f:
         # dump an empty dictionary
         yaml.dump({}, f)
